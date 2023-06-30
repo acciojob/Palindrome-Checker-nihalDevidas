bc780c4cad4a911ffa5d21e6a6ec6b57a4839c59
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-06-30 16:58:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // complete the given function
 
 function palindrome(str){
+	let newString = "";
+	
+	for(let i=0; i<str.length;i++){
+	    let val = str.charAt(i);
+	    if(val !=" "){
+	      newString = newString+val;
+	    }
+	}
+	newString = newString.toLowerCase();
+	
+	
+	let start = 0; let end = newString.length-1; let ispalindrome = true;
+	
+	while(start <= end){
+	    if(newString.charAt(start) != newString.charAt(end) ){
+	        ispalindrome = false;
+	        break;
+	    }
+	    start++;
+	    end--;
+	}
+	let output = ispalindrome ? true:false;
 
+ return output;	
 }
 module.exports = palindrome
